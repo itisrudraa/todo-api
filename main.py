faf89003ff8a17c00819891417c0afbea2eb646e
itisrudraa <merudra01@gmail.com>
2026-08-10 18:38:06 +0530

    feat: add GET and POST endpoints for todos

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from fastapi import FastAPI
 from data import todos
-
+from models import Createtodo
 app = FastAPI()
 
 @app.get("/")
 def home():
     return {"message": "server has started"}
 
+@app.get("/todos")
+def get_todos():
+    return todos
 
+@app.post("/create")
+def create_todo(newtodo: Createtodo):
+    new_id = len(todos)+1
+    todos[new_id] = newtodo.model_dump()
+
+    return {
+        "todo": todos[new_id],
+        "message" : "todo created"
+    }
+    
 
