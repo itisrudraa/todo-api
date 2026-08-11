9df8f4be05a825022ae6eb773d01110b8df60562
itisrudraa <merudra01@gmail.com>
2026-08-11 17:39:09 +0530

    Feat: add edit mark and delete todo endpoints

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"message": "server has started"}
-
+  
 @app.get("/todos")
 def get_todos():
     return todos
@@ -20,5 +20,56 @@ def create_todo(newtodo: Createtodo):
         "todo": todos[new_id],
         "message" : "todo created"
     }
-    
 
+
+@app.patch("/mark/{todo_id}")
+def mark(todo_id: int):
+    if todo_id in todos:
+        todo = todos[todo_id]
+        todo["completed"] = True
+
+        return {
+            "todo": todo,
+            "message" : "Maked"
+        }
+    else:
+        return{
+            "message": "invalid todo id"
+        }
+    
+@app.patch("/edit/{todo_id}")
+def edit_title(todo_id: int, title: str):
+    if todo_id in todos:
+        todo = todos[todo_id]
+        todo["title"] = title
+
+        return {
+            "todo": todo,
+            "message" : "title edited"
+        }
+    else:
+        return{
+            "message": "invalid todo id"
+        }
+
+@app.get("/todos/{todo_id}")
+def get_todo(todo_id: int):
+    if todo_id in todos:
+        return todos[todo_id]
+    else:
+        return{
+            "message": "invalid todo id"
+        }
+
+@app.delete("/todos/{todo_id}")
+def delete_todo(todo_id: int):
+    if todo_id in todos:
+        todo = todos.pop(todo_id, None)
+        return{
+            "todo": todo,
+            "message": "todo deleted"
+        }
+    else:
+        return{
+            "message": "invalid todo id"
+        }
