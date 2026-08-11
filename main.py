610ea4fa138a42715934ca88f364174465605088
itisrudraa <merudra01@gmail.com>
2026-08-11 18:22:06 +0530

    implement todo CRUD endpoints

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from fastapi import FastAPI
-from data import todos
-from models import Createtodo
+from fastapi import FastAPI, HTTPException
+import data
+from models import Createtodo, Updatetodo
 app = FastAPI()
 
 @app.get("/")
@@ -9,23 +9,24 @@ def home():
   
 @app.get("/todos")
 def get_todos():
-    return todos
+    return data.todos
 
-@app.post("/create")
+@app.post("/todos")
 def create_todo(newtodo: Createtodo):
-    new_id = len(todos)+1
-    todos[new_id] = newtodo.model_dump()
+    new_id = data.next_id
+    data.next_id += 1
+    data.todos[new_id] = newtodo.model_dump()
 
     return {
-        "todo": todos[new_id],
+        "todo": data.todos[new_id],
         "message" : "todo created"
     }
 
 
 @app.patch("/mark/{todo_id}")
 def mark(todo_id: int):
-    if todo_id in todos:
-        todo = todos[todo_id]
+    if todo_id in data.todos:
+        todo = data.todos[todo_id]
         todo["completed"] = True
 
         return {
@@ -33,43 +34,48 @@ def mark(todo_id: int):
             "message" : "Maked"
         }
     else:
-        return{
-            "message": "invalid todo id"
-        }
+        raise HTTPException(
+            status_code=404,
+            detail="Invalid todo id"
+        )
     
 @app.patch("/edit/{todo_id}")
-def edit_title(todo_id: int, title: str):
-    if todo_id in todos:
-        todo = todos[todo_id]
-        todo["title"] = title
+def edit_title(todo_id: int, ntodo: Updatetodo):
+    if todo_id in data.todos:
+        todo = data.todos[todo_id]
+        tdo = ntodo.model_dump()
+        todo["title"] = tdo["title"]
 
         return {
             "todo": todo,
             "message" : "title edited"
         }
     else:
-        return{
-            "message": "invalid todo id"
-        }
+        raise HTTPException(
+            status_code=404,
+            detail="Invalid todo id"
+        )
 
 @app.get("/todos/{todo_id}")
 def get_todo(todo_id: int):
-    if todo_id in todos:
-        return todos[todo_id]
+    if todo_id in data.todos:
+        return data.todos[todo_id]
     else:
-        return{
-            "message": "invalid todo id"
-        }
+        raise HTTPException(
+            status_code=404,
+            detail="Invalid todo id"
+        )
 
 @app.delete("/todos/{todo_id}")
 def delete_todo(todo_id: int):
-    if todo_id in todos:
-        todo = todos.pop(todo_id, None)
+    if todo_id in data.todos:
+        todo = data.todos.pop(todo_id, None)
         return{
             "todo": todo,
             "message": "todo deleted"
         }
     else:
-        return{
-            "message": "invalid todo id"
-        }
+        raise HTTPException(
+            status_code=404,
+            detail="Invalid todo id"
+        )
