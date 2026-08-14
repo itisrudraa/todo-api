4ded98ab58d6785b7496699c4b833ba272aca4f5
itisrudraa <merudra01@gmail.com>
2026-08-14 16:32:11 +0530

    refactor toto routes using prefix

--- routes/todos.py
+++ routes/todos.py
@@ -3,7 +3,7 @@ from models import Createtodo, Updatetodo, todoResponse, Todo
 import data
 
 
-router = APIRouter(tags=["Todos"])
+router = APIRouter(prefix="/todos", tags=["Todos"])
 
 def get_todo_dependency(todo_id: int):
     if todo_id not in data.todos:
@@ -14,11 +14,11 @@ def get_todo_dependency(todo_id: int):
     todo = data.todos[todo_id]
     return todo
 
-@router.get("/todos")
+@router.get("")
 def get_todos():
     return data.todos
 
-@router.post("/todos", response_model=todoResponse)
+@router.post("", response_model=todoResponse)
 def create_todo(newtodo: Createtodo):
     new_id = data.next_id
     data.next_id += 1
@@ -30,7 +30,7 @@ def create_todo(newtodo: Createtodo):
     }
 
 
-@router.patch("/todos/{todo_id}", response_model=todoResponse)
+@router.patch("/{todo_id}", response_model=todoResponse)
 def update_todo(update: Updatetodo, todo = Depends(get_todo_dependency)):
 
     if update.title is not None:
@@ -44,11 +44,11 @@ def update_todo(update: Updatetodo, todo = Depends(get_todo_dependency)):
         "message": "todo updated"
     }
 
-@router.get("/todos/{todo_id}", response_model=Todo)
+@router.get("/{todo_id}", response_model=Todo)
 def get_todo(todo = Depends(get_todo_dependency)):
     return todo
 
-@router.delete("/todos/{todo_id}", response_model=todoResponse)
+@router.delete("/{todo_id}", response_model=todoResponse)
 def delete_todo(todo_id: int, todo = Depends(get_todo_dependency)):
     data.todos.pop(todo_id)
     return{
