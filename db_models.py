24e6c9fd465da841e0cee0f5974adbaa6305d7e0
itisrudraa <merudra01@gmail.com>
2026-08-18 19:40:19 +0530

    feat: add user model and todo relationships

--- db_models.py
+++ db_models.py
@@ -1,4 +1,5 @@
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
 class Base(DeclarativeBase):
     pass
@@ -10,3 +11,17 @@ class Todo(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     title: Mapped[str]
     completed: Mapped[bool]
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+
+    user: Mapped["User"] = relationship(back_populates="todos")
+
+class User(Base):
+
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str]
+    email: Mapped[str]
+    password_hash: Mapped[str]
+
+    todos: Mapped[list["Todo"]] = relationship(back_populates="user")
