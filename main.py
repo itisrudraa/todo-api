2db052ae4ddf144472a6fdc2f5f19c2c0c080257
itisrudraa <merudra01@gmail.com>
2026-08-21 00:12:36 +0530

    add auth router

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from routes.todos import router
-
+from routes.auth import auth_router
 app = FastAPI()
 
 @app.get("/")
@@ -8,3 +8,4 @@ def home():
     return {"message": "server has started"}
   
 app.include_router(router)
+app.include_router(auth_router)
