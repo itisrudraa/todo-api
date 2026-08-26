709f6745d2a7c1ff2bf898a725c03df5674564f0
itisrudraa <merudra01@gmail.com>
2026-08-26 13:24:58 +0530

    implement route protection

--- routes/todos.py
+++ routes/todos.py
@@ -1,17 +1,17 @@
 from fastapi import APIRouter, HTTPException, Depends
 from models import Createtodo, Updatetodo, todoResponse, Todo
-from database import get_all_todos, createTodo, updateTodo, getTodo, deleteTodo, get_db
-
+from database import get_all_todos, createTodo, updateTodo, getTodo, deleteTodo, get_db, getUser
+from utils.helper import is_authenticated
 
 router = APIRouter(prefix="/todos", tags=["Todos"])
 
 @router.get("", response_model=list[Todo])
-def get_todos(db=Depends(get_db)):
-    return get_all_todos(db)
+def get_todos(user=Depends(is_authenticated)):
+    return get_all_todos(user)
 
 @router.post("", response_model=todoResponse)
-def create_todo(newtodo: Createtodo, db=Depends(get_db)):
-    todo = createTodo(newtodo.title, newtodo.completed, db)
+def create_todo(newtodo: Createtodo, db=Depends(get_db), user=Depends(is_authenticated)):
+    todo = createTodo(newtodo.title, newtodo.completed, user.id, db)
 
     return{
         "todo": todo,
@@ -20,9 +20,9 @@ def create_todo(newtodo: Createtodo, db=Depends(get_db)):
 
 
 @router.patch("/{todo_id}", response_model=todoResponse)
-def update_todo(todo_id: int, update: Updatetodo, db=Depends(get_db)):
+def update_todo(todo_id: int, update: Updatetodo, db=Depends(get_db), user = Depends(is_authenticated)):
 
-    todo = updateTodo(todo_id, update.title, update.completed, db)
+    todo = updateTodo(todo_id, update.title, update.completed, db, user.id)
     if todo is None:
         raise HTTPException(
             status_code=404,
@@ -35,8 +35,8 @@ def update_todo(todo_id: int, update: Updatetodo, db=Depends(get_db)):
     }
 
 @router.get("/{todo_id}", response_model=Todo)
-def get_todo(todo_id: int, db=Depends(get_db)):
-    todo = getTodo(todo_id, db)
+def get_todo(todo_id: int, db=Depends(get_db), user=Depends(is_authenticated)):
+    todo = getTodo(todo_id, db, user.id)
     if todo is None:
         raise HTTPException(
             status_code=404,
@@ -46,8 +46,8 @@ def get_todo(todo_id: int, db=Depends(get_db)):
     return todo
 
 @router.delete("/{todo_id}", response_model=todoResponse)
-def delete_todo(todo_id: int, db=Depends(get_db)):
-    todo = deleteTodo(todo_id, db)
+def delete_todo(todo_id: int, user=Depends(is_authenticated), db=Depends(get_db)):
+    todo = deleteTodo(todo_id, db, user.id)
     if todo is None:
         raise HTTPException(
             status_code=404,
